c50c3706bbd3b40b4972d8821d319183dcb94532
Jm <agilestar8@gmail.com>
2022-01-07 03:57:31 +0900

    graph

---
+++ baekjoon/그래프 이론/10026.py
@@ -0,0 +1,53 @@
+import sys
+input = sys.stdin.readline
+sys.setrecursionlimit(10000)
+
+n = int(input())
+graph = [list(input()) for _ in range(n)]
+dx = [1,0,-1,0]
+dy = [0,1,0,-1]
+
+cnt = 0
+cnt2 = 0
+
+def DFS(x,y):
+    visit[x][y] = cnt
+    for i in range(4):
+        nx = x + dx[i]
+        ny = y + dy[i]
+        if nx>=0 and nx<n and ny>=0 and ny<n and graph[nx][ny] == graph[x][y] and not visit[nx][ny]:
+            visit[nx][ny] = 1
+            DFS(nx,ny)
+
+
+visit = [[False]*n for _ in range(n)]
+for i in range(n):
+    for j in range(n):
+        if not visit[i][j]:
+            cnt += 1 
+            DFS(i,j)
+            
+
+visit = [[False]*n for _ in range(n)]
+for i in range(n):
+    for j in range(n):
+        if graph[i][j] == "R":
+            graph[i][j] = "G"
+
+for i in range(n):
+    for j in range(n):
+        if not visit[i][j]:
+            cnt2 += 1
+            DFS(i,j)
+
+
+print(cnt, cnt2)
+
+
+
+
+            
+
+            
+            
+            
--- baekjoon/그래프 이론/11403.py
+++ baekjoon/그래프 이론/11403.py
@@ -1,22 +1,21 @@
 n = int(input()) # 정점의 개수
 graph = [list(map(int,input().split())) for _ in range(n)] # 입력은 인접행렬 방식
 
+# 플루이드 와셜
 for k in range(n):
     for i in range(n):
         for j in range(n):
-            if graph[i][j] == 1 or (graph[i][k] ==1 and graph[k][j] == 1):
+            
+            # 바로 i -> j가 되거나, i -> k -> j가 가능하다면 
+            # i -> j로가는 선이 존재한다는 것
+            if graph[i][j] == 1 or (graph[i][k] ==1 and graph[k][j] == 1):                 
                 graph[i][j] = 1
             
 for i in range(n):
     for j in range(n):
         print(graph[i][j], end = " ")
     print()
-            
-# 3
-# 0 1 0
-# 0 0 1
-# 1 0 0
-
+   
 
 
 

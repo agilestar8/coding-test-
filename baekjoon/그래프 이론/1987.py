7e054bcf71c4f9ab41f089bff8d07a0140a2ecb3
Jm <agilestar8@gmail.com>
2022-01-10 04:13:16 +0900

    graph

--- baekjoon/그래프 이론/1987.py
+++ baekjoon/그래프 이론/1987.py
@@ -12,16 +12,17 @@ def BFS(x,y):
     cnt = 1
         
     while q:
+        # print(q)
         x,y,alpha = q.pop()
+        
         for i in range(4):
             nx = x + dx[i]
             ny = y + dy[i]
             if nx>=0 and nx<r and ny>=0 and ny<c and graph[nx][ny] not in alpha:
-                    q.add((nx, ny, graph[nx][ny]+alpha )) # set에 알파벳을 추가하는데, 중복알파벳 일시 추가안돰
-                    cnt = max(cnt, len(alpha)+1)
+                    q.add((nx, ny, graph[nx][ny]+alpha )) # set에 알파벳을 추가하는데, 중복알파벳 일시 추가안됨
+                    cnt = max(cnt, len(alpha)+1) 
     
     print(cnt)
 
 BFS(0,0)
 
-    
---
+++ baekjoon/그래프 이론/2252.py
@@ -0,0 +1,44 @@
+import sys
+from collections import deque
+input = sys.stdin.readline
+
+# 위상정렬 알고리즘
+# 위상정렬의 과정은 다음과 같다.
+
+# 1.진입차수가 0인 정점을 큐에 삽입
+# 2.큐에서 원소를 꺼내 해당 원소와 연결된 간선을 모두 제거
+# 3.제거한 후에 진입차수가 0인 정점을 큐에 삽입
+# 4.이후 2~3의 과정을 반복
+
+n,m = map(int,input().split())
+
+graph = [[] for _ in range(n+1)]
+inDegree = [0]*(n+1)   # 1번부터 n번까지 진입차수
+q = deque([])
+ans = []
+
+# 인접리스트로 그래프 구성
+for _ in range(m):
+    a,b = map(int,input().split())
+    graph[a].append(b)  # a에서 b로 가는 간선있음
+    inDegree[b] += 1    # b의 진입차수 +1 (순서뒤로)
+
+# 위상정렬
+def topological_sorting():
+
+    for i in range(1,n+1):
+        if inDegree[i] == 0:    # 진입차수가 0이면
+            q.append(i)         # 앞 순서
+        
+    while q:
+        v = q.popleft()
+        ans.append(v)           # 진입차수0이므로 먼저 나옴
+        
+        for i in graph[v]:      # 나온 노드의 인접노드들의
+            inDegree[i] -= 1    # 간선을 제거(진입차수 감소)
+
+            if inDegree[i] == 0:# 진입차수가 0이면 
+                q.append(i)     # 큐에 추가
+
+topological_sorting()
+print(*ans)    

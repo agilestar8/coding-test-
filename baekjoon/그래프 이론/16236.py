6e3153fdf49e95d3869a96196811cc1eb9eced1d
Jm <agilestar8@gmail.com>
2022-01-18 23:41:47 +0900

    graph

--- baekjoon/그래프 이론/16236.py
+++ baekjoon/그래프 이론/16236.py
@@ -1,4 +1,3 @@
-from pickle import FALSE
 import sys
 input = sys.stdin.readline
 from collections import deque
@@ -9,38 +8,48 @@ visit = [[0]*n for i in range(n)]
 dx = [-1,0,1,0]
 dy = [0,-1,0,1]
 
+shark_size = 2 
+eat_cnt = 0
+fish_cnt = 0
+fish_pos = []
+
+for i in range(n):
+    for j in range(n):
+        if graph[i][j] == 9:
+            q.
+           
+for i in range(n):
+    for j in range(n):
+        if 0 < graph[i][j] <=6:     # 물고기가 있으면
+            fish_cnt +=1            # cnt +1
+            fish_pos.append((i,j))  # 위치 기록
+            
+        elif graph[i][j] == 9:      # 상어의
+            shark_x, shark_y = i,j  # 위치 기록
+            graph[shark_x][shark_y]=0   # 잠시 비어놓음
+
 
 def bfs(x,y):
-    q = deque([(x,y)])
-    time = 0
-    shark_size = 2 
+    q = deque([(x,y,0)])
+    visit[x][y] = 1
+    arr = []
+    min_dist = 1e9
     
+
     while q:
-        x,y = q.popleft() 
+        x,y,dist = q.popleft() 
+                
         for i in range(4):
             nx = x + dx[i]
             ny = y + dy[i]            
-            if 0<=nx<n and 0<=ny<n:
-                if graph[nx][ny] < shark_size:      # 상어가 더 크면
-                    graph[nx][ny] = 0               # 잡아먹고 
-                    visit[nx][ny] = visit[x][y] + 1 # 거리 1증가
-                    time += 1                       # 시간 1증가
-                    q.append((nx,ny))               # 위치이동
-            
-                elif graph[nx][ny] == shark_size:   # 같으면
-                    time += 1
+            if 0<=nx<n and 0<=ny<n and not visit[nx][ny]:
+                if graph[nx][ny] <= shark_size:   # 물고기가 상어보다 작으면
+                    visit[nx][ny] = 1           # 이동
+                    if 0<graph[nx][ny]<shark_size:  # 
+                        min_dist = dist
+                        arr.append((dist+1,nx,ny))
+                    if  dist+1 <= min_dist:
+                        q.append((nx,ny,dist+1))
                     
-                elif graph[nx][ny] > shark_size:    # 물고기가 더 크면
-                    time += 1
-                    visit[nx][ny] = visit[x][y] + 1 # 거리만 1증가
                     
-                
-                
-fish = False                    
-for i in range(n):
-    for j in range(n):
-        if graph[i][j] != 0 and graph[i][j] != 9:
-            fish = True
-            break
-
-
+        

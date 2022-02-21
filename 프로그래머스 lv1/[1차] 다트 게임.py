775f1278681d085b1a0240d1d4826afd2c9395c4
Jm <agilestar8@gmail.com>
2022-02-22 01:33:50 +0900

    level1

--- 프로그래머스 lv1/[1차] 다트 게임.py
+++ 프로그래머스 lv1/[1차] 다트 게임.py
@@ -1,47 +1,32 @@
 def solution(dartResult):
-    answer = 0
-    n = len(dartResult)
-    dartResult = list(dartResult)
 
-    # str을 list로
     arr = []
     temp = ""
     for i in dartResult:
-        if i.isalpha():
-            if len(temp) > 0:
-                arr.append(temp)
-                arr.append(i)
-                temp = ""
-            else:
-                arr.append(i)
-
-        elif i.isdigit():
+        if i.isdigit():
             temp += i
-        else:
-            arr.append(i)
             
-    print(arr)
-
-    # 숫자만 stack에 넣기
-    stack = []
-    for i in range(len(arr)):
-        if arr[i] == 'S':
-            stack.append(int(arr[i-1])) # ^1배
-                    
-        elif arr[i] == 'D':
-            stack.append(int(arr[i-1])**2) # ^2배
-        
-        elif arr[i] == 'T':
-            stack.append(int(arr[i-1])**3) # ^3배
-    
-        elif arr[i] == '*': # 맨뒤에서 2개 2배
-            stack[-1] = stack[-1]*2
-            if len(stack) > 1:            
-                stack[-2] = stack[-2]*2
-        
-        elif arr[i] == '#': # 현재숫자 -1
-            stack[-1] = -stack[-1]
+        elif i == 'S':
+            a = int(temp)**1
+            arr.append(a)
+            temp = ""
             
-    print(stack)
-    print(sum(stack))
-    return (sum(stack))
+        elif i == 'D':
+            a = int(temp)**2
+            arr.append(a)
+            temp = ""
+            
+        elif i == 'T':
+            a = int(temp)**3
+            arr.append(a)
+            temp = ""
+            
+        elif i == '#':
+            arr[-1] = -arr[-1]
+            
+        elif i == '*':
+            arr[-1] = arr[-1]*2
+            if len(arr) > 1:
+                arr[-2] = arr[-2]*2
+                
+    return sum(arr)

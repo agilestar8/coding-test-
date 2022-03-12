5d9d38dcdbfebd2c6a78adb1775327f3fad78719
Jm <agilestar8@gmail.com>
2022-03-13 00:45:18 +0900

    algorithm

---
+++ baekjoon/동적계획법/1003피보나치.py
@@ -0,0 +1,21 @@
+import sys
+input = sys.stdin.readline
+
+t = int(input())
+for _ in range(t):
+    n = int(input())
+    if n == 0:
+        print(0)
+        continue
+    elif n == 1:
+        print(1)
+        continue
+    
+    dp = [0]*(n+1)
+    dp[0] = 0
+    dp[1] = 1
+    
+    for i in range(2,n+1):
+        dp[i] = dp[i-1] + dp[i-2]
+        
+    

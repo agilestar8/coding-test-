0a652da13fc240396965e4f7e229b82c11431df6
조민규 <agilestar8@gmail.com>
2022-04-05 13:49:09 +0900

    algorithm

--- 프로그래머스 lv3/징검다리 건너기.py
+++ 프로그래머스 lv3/징검다리 건너기.py
@@ -1,10 +1,36 @@
 # 연속된 0돌 개수가 k이하여야한다
 # answer
+def solution(stones, k):
+    answer = 0
+    # 연속된 0돌 개수가 k이하여야한다 
+    
+    left = 1 # 최소
+    right =  200000000# 최대 
+    
+    while left <= right: 
+        mid = (left+right)//2
 
+        # 연속되는 0돌 체크
+        seq = 0
+        for i in stones:
+            if i <= mid:
+                seq += 1
+            else:
+                seq = 0
+            
+            if seq >= k: # k를 넘는순간 중지해야 시간초과안걸림, 다 구하고 max()는 시간초과
+                break
+                    
+        if seq < k:       # 감당가능하면 
+            left = mid+1  # 사람 수 늘려보고
+        else:             # 감당불가능하면
+            right = mid-1 # 사람 수 줄여보기
+                          
+    return left
 
 
 # case2 
-def solution(stones, k):
+def solution2(stones, k):
     answer = 0
     n = len(stones)
         

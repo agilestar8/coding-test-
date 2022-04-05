ac1ba28c99b901be307464c2bfcb08008ec74127
조민규 <agilestar8@gmail.com>
2022-04-05 13:12:07 +0900

    git ignore

--- 프로그래머스 lv3/징검다리 건너기.py
+++ 프로그래머스 lv3/징검다리 건너기.py
@@ -1,1 +1,25 @@
-print(1)
+# 연속된 0돌 개수가 k이하여야한다
+# answer
+
+
+
+# case2 
+def solution(stones, k):
+    answer = 0
+    n = len(stones)
+        
+    
+    while True:  
+        answer += 1
+        for i in range(n):
+            if stones[i] > 0:
+                stones[i] -= 1
+
+        zcnt = 0
+        for j in stones:
+            if j == 0:
+                zcnt += 1
+                if zcnt == k:
+                    return answer
+            else:
+                zcnt = 0

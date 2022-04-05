83c2555fc157589d0b0262f1e70a7adab5c848c7
Jm <agilestar8@gmail.com>
2022-04-06 00:22:09 +0900

    algorithm

--- 프로그래머스 lv3/보석 쇼핑.py
+++ 프로그래머스 lv3/보석 쇼핑.py
@@ -1,25 +1,31 @@
 def solution(gems):
-    N = len(gems)
-    answer = [0, N-1]
-    kind = len(set(gems))  # 보석종류
-    dic = {gems[0]:1,}  # 종류 체크할 딕셔너리
-    s,e = 0,0  # 투포인터
-    while s<N and e<N:
-        if len(dic) < kind:  # 종류 부족하면 end point 증가 & dic 개수 증가
-            e += 1
-            if e == N:
-                break
-            dic[gems[e]] = dic.get(gems[e], 0) + 1
-            
-        else:  # 종류 같으면 ans 비교 후 답 갱신하고, start point 증가 & dic 개수 다운
-            if (e-s+1) < (answer[1]-answer[0]+1):
-                answer = [s,e]
-            if dic[gems[s]] == 1:
-                del dic[gems[s]]
-            else:
-                dic[gems[s]] -= 1
-            s += 1
-
-    answer[0] += 1
-    answer[1] += 1
+    answer = []
+    # 투 포인터
+    n = len(gems)
+    gem_kind = len(set(gems))
+    i,j = 0,0
+    dic = {}
+    
+    min_len = n+1
+    while j<n: 
+        if gems[j] not in dic:
+            dic[gems[j]] = 1
+        else:
+            dic[gems[j]] += 1
+        j += 1
+        
+        if gem_kind == len(dic):
+            while i<n:
+                if dic[gems[i]] > 1:
+                    dic[gems[i]] -= 1
+                    i += 1
+                    
+                elif j-i < min_len: # 더 최소길이라면 
+                    min_len = j-i
+                    answer = [i+1,j]
+                    break   # 종료
+                
+                else: # 한 보석이 1개밖에 없을 때(없애면 종류줄으므로) 
+                    break   # 종료
+                
     return answer
